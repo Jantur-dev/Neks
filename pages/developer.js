a3af77a502c1aa9f76dd7555c3d127ca51bc1159
Jantur-dev <jantur29@gmail.com>
2022-12-07 14:08:00 +0700

    Update Developer Instagram

--- pages/developer.js
+++ pages/developer.js
@@ -14,7 +14,7 @@ const developer = () => {
                     <div class="p-6">
                         <h5 class="text-gray-900 text-xl font-medium mb-2">Alfat T N</h5>
                         <p class="text-gray-700 text-base mb-4">PHP itu kunoq</p>
-                        <Link href={"https://instagram.com/rubbnms"} target={"_blank"}>
+                        <Link href={"https://instagram.com/alfattaufikn"} target={"_blank"}>
                             <button type="button" class="inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-white hover:text-black hover:shadow-lg focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 active:bg-black active:shadow-lg transition duration-700">Instagram</button>
                         </Link>
                     </div>
@@ -31,7 +31,7 @@ const developer = () => {
                     <div class="p-6">
                         <h5 class="text-gray-900 text-xl font-medium mb-2">Gabriel N H</h5>
                         <p class="text-gray-700 text-base mb-4">Gabjes</p>
-                        <Link href={"https://instagram.com/rubbnms"} target={"_blank"}>
+                        <Link href={"https://instagram.com/gab_nh27"} target={"_blank"}>
                             <button type="button" class="inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-white hover:text-black hover:shadow-lg focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 active:bg-black active:shadow-lg transition duration-700">Instagram</button>
                         </Link>
                     </div>
@@ -48,7 +48,7 @@ const developer = () => {
                     <div class="p-6">
                         <h5 class="text-gray-900 text-xl font-medium mb-2">Ikhsandi S</h5>
                         <p class="text-gray-700 text-base mb-4">San</p>
-                        <Link href={"https://instagram.com/rubbnms"} target={"_blank"}>
+                        <Link href={"https://instagram.com/ikhsandi_1110"} target={"_blank"}>
                             <button type="button" class="inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-white hover:text-black hover:shadow-lg focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 active:bg-black active:shadow-lg transition duration-700">Instagram</button>
                         </Link>
                     </div>
@@ -65,7 +65,7 @@ const developer = () => {
                     <div class="p-6">
                         <h5 class="text-gray-900 text-xl font-medium mb-2">M Farrel A</h5>
                         <p class="text-gray-700 text-base mb-4">M F A</p>
-                        <Link href={"https://instagram.com/rubbnms"} target={"_blank"}>
+                        <Link href={"https://instagram.com/farrelll_ar"} target={"_blank"}>
                             <button type="button" class="inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-white hover:text-black hover:shadow-lg focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 active:bg-black active:shadow-lg transition duration-700">Instagram</button>
                         </Link>
                     </div>
@@ -99,7 +99,7 @@ const developer = () => {
                     <div class="p-6">
                         <h5 class="text-gray-900 text-xl font-medium mb-2">S. Al Fahruz z.</h5>
                         <p class="text-gray-700 text-base mb-4">Shobib</p>
-                        <Link href={"https://instagram.com/rubbnms"} target={"_blank"}>
+                        <Link href={"https://instagram.com/manusia_limited"} target={"_blank"}>
                             <button type="button" class="inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-white hover:text-black hover:shadow-lg focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 active:bg-black active:shadow-lg transition duration-700">Instagram</button>
                         </Link>
                     </div>
